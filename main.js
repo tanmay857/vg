dc03bf5c8024bf8f0e2750d5b22bdce9ceab15b4
tanmay857 <84132588+tanmay857@users.noreply.github.com>
2021-05-22 10:01:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function home() {
+    window.alert ("My name is sir timberners lee.I am the founder of w.w.w : world wide web") }
+function inspiration() {
+    window.alert( "A graduate of Oxford University, Sir Tim invented the World Wide Web at CERN, the European Particle Physics Laboratory. He wrote the first web client and server. His specifications of URIs, HTTP and HTML were refined as Web technology spread." )
+}
+function work() {
+    window.alert ( "Sir Tim Berners-Lee invented the World Wide Web in 1989. He is the co-founder and CTO of Inrupt.com, a tech start-up which uses, promotes and helps develop the open source Solid platform. Solid aims to give people control and agency over their data, questioning many assumptions about how the web has to work." )
+}
+function writings() {
+    window.alert ( "A graduate of Oxford University, Sir Tim invented the Web while at CERN, the European Particle Physics Laboratory, in 1989. He wrote the first web client and server in 1990. His specifications of URIs, HTTP and HTML were refined as Web technology spread." )
+}
+function contact() {
+    window.alert ( "n/a" )
+}
